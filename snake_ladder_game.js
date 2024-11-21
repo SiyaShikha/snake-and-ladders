d5f4671adf1a5087d09294c03c14939c95b3af45
Shikha Singh <150659967+SiyaShikha@users.noreply.github.com>
2024-11-21 11:35:20 +0530

    first version of snake and ladder game.

---
+++ snake_ladder_game.js
@@ -0,0 +1,139 @@
+function repeat(string, nTimes) {
+  if (nTimes <= 0) {
+    return "";
+  }
+
+  return string + repeat(string, nTimes - 1);
+}
+
+function getBox(string) {
+  const line = repeat("―", string.length);
+  return line + "\n" + string + "\n" + line;
+}
+
+function rollDice() {
+  return Math.ceil(Math.random() * 6);
+}
+
+function snakeFound(position) {
+  console.log("🐍 Ops! Snake has bit you.🐍")
+  switch (position) {
+    case 28:
+      return 10;
+    case 37:
+      return 3;
+    case 48:
+      return 16;
+    case 75:
+      return 32;
+    case 94:
+      return 71;
+    case 96:
+      return 42;
+  }
+}
+
+function ladderFound(position) {
+  console.log("🪜 Hey! You got a Ladder.🪜")
+  switch (position) {
+    case 4:
+      return 56;
+    case 12:
+      return 50;
+    case 14:
+      return 55;
+    case 22:
+      return 58;
+    case 41:
+      return 79;
+    case 54:
+      return 88;
+  }
+}
+
+function nextPosition(position, rolledNum) {
+  if (position > 100) {
+    position = position - rolledNum;
+  }
+
+  switch (position) {
+    case 28:
+    case 37:
+    case 48:
+    case 75:
+    case 94:
+    case 96: return snakeFound(position);
+    case 4:
+    case 12:
+    case 14:
+    case 22:
+    case 41:
+    case 54: return ladderFound(position);
+    default: return position;
+  }
+}
+
+function player1(p1Position) {
+  const p1CurrentPosition = getBox("Current Position : " + p1Position);
+  console.log("\n\n" + "🧑🏻‍💼 Player1\n" + p1CurrentPosition);
+  prompt("Hit return to roll the dice 🎲");
+  const p1RolledNum = rollDice();
+  console.log("Rolled number :", p1RolledNum);
+  p1Position += p1RolledNum;
+
+  p1Position = nextPosition(p1Position, p1RolledNum);
+
+  console.log(getBox("New Position     : " + p1Position));
+  return p1Position;
+}
+
+function player2(p2Position) {
+  const p2CurrentPosition = getBox("Current Position : " + p2Position);
+  console.log("\n\n" + "👩🏻‍💼 Player2\n" + p2CurrentPosition);
+  prompt("Hit return to roll the dice 🎲");
+  const p2RolledNum = rollDice();
+  console.log("Rolled number :", p2RolledNum);
+  p2Position += p2RolledNum;
+
+  p2Position = nextPosition(p2Position, p2RolledNum);
+
+  console.log(getBox("New Position     : " + p2Position));
+  return p2Position;
+}
+
+function startPlaying() {
+  let p1Position = 0;
+  let p2Position = 0;
+
+  while (!(p1Position === 100 || p2Position === 100)) {
+    // Player1
+    p1Position = player1(p1Position);
+
+    if (p1Position === 100) {
+      console.log("\n🏆Congratulations! Player1 won the Game. 🏆\n");
+      break;
+    }
+
+    // Player2
+    p2Position = player2(p2Position);
+
+    if (p2Position === 100) {
+      console.log("\n🏆Congratulations! Player2 won the Game. 🏆\n");
+    }
+  }
+
+  if (confirm("\nDo you want to play new game?")) {
+    startPlaying();
+  } else {
+    console.log("\nGoodBye!\n");
+  }
+}
+
+console.log(getBox("🐍🪜 WELCOME TO SNAKE AND LADDER GAME 🪜🐍"));
+const start = confirm("\nDo you want to start the game?");
+
+if (start) {
+  startPlaying();
+} else {
+  console.log(getBox("\nGoodBye!\n"));
+} 
